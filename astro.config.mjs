37a93a89e82d5ce14e0a80a26458e9da7dd81658
Cricket-Engineer <paul97bsc@gmail.com>
2025-08-13 12:51:26 -0500

    Configuracion del GitPages 1.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://Cricket-Engineer.github.io/PortafolioPaulPasaca/',
-  base: '/PortafolioPaulPasaca/', // << importante para rutas
-  output: 'static',
+  site: "https://Cricket-Engineer.github.io/PortafolioPaulPasaca/",
+  base: "/PortafolioPaulPasaca/",
 });
+
